3f3f32f70a882ae6108b4fec14272de13ab0e43c
Aidan-Towers <103062627+Aidan-Towers@users.noreply.github.com>
2024-02-05 14:36:04 -0600

    Add files via upload

---
+++ Hangman/Hangman/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Hangman
+{
+    partial class Hangman
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.letterInput = new System.Windows.Forms.TextBox();
+            this.hangmanPicture = new System.Windows.Forms.PictureBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.letterGuessButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.wordInput = new System.Windows.Forms.TextBox();
+            this.wordGuessButton = new System.Windows.Forms.Button();
+            this.realWordLabel = new System.Windows.Forms.Label();
+            this.winOrLoseLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lettersGuessed = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // letterInput
+            // 
+            this.letterInput.Enabled = false;
+            this.letterInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.letterInput.Location = new System.Drawing.Point(86, 299);
+            this.letterInput.MaxLength = 1;
+            this.letterInput.Name = "letterInput";
+            this.letterInput.Size = new System.Drawing.Size(61, 30);
+            this.letterInput.TabIndex = 0;
+            this.letterInput.TextChanged += new System.EventHandler(this.letterInput_TextChanged);
+            // 
+            // hangmanPicture
+            // 
+            this.hangmanPicture.Image = global::Hangman.Properties.Resources.Base;
+            this.hangmanPicture.Location = new System.Drawing.Point(257, 131);
+            this.hangmanPicture.Name = "hangmanPicture";
+            this.hangmanPicture.Size = new System.Drawing.Size(573, 501);
+            this.hangmanPicture.TabIndex = 1;
+            this.hangmanPicture.TabStop = false;
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.startButton.Location = new System.Drawing.Point(890, 290);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(133, 48);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start Game";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Enabled = false;
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.resetButton.Location = new System.Drawing.Point(890, 483);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(133, 48);
+            this.resetButton.TabIndex = 3;
+            this.resetButton.Text = "Reset Game";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(48, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Guess a letter:";
+            // 
+            // letterGuessButton
+            // 
+            this.letterGuessButton.Enabled = false;
+            this.letterGuessButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.letterGuessButton.Location = new System.Drawing.Point(53, 335);
+            this.letterGuessButton.Name = "letterGuessButton";
+            this.letterGuessButton.Size = new System.Drawing.Size(133, 48);
+            this.letterGuessButton.TabIndex = 6;
+            this.letterGuessButton.Text = "Guess Letter";
+            this.letterGuessButton.UseVisualStyleBackColor = true;
+            this.letterGuessButton.Click += new System.EventHandler(this.letterGuessButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(42, 448);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Guess the word:";
+            // 
+            // wordInput
+            // 
+            this.wordInput.Enabled = false;
+            this.wordInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.wordInput.Location = new System.Drawing.Point(29, 483);
+            this.wordInput.MaxLength = 1;
+            this.wordInput.Name = "wordInput";
+            this.wordInput.Size = new System.Drawing.Size(191, 30);
+            this.wordInput.TabIndex = 8;
+            this.wordInput.TextChanged += new System.EventHandler(this.wordInput_TextChanged);
+            // 
+            // wordGuessButton
+            // 
+            this.wordGuessButton.Enabled = false;
+            this.wordGuessButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.wordGuessButton.Location = new System.Drawing.Point(55, 521);
+            this.wordGuessButton.Name = "wordGuessButton";
+            this.wordGuessButton.Size = new System.Drawing.Size(133, 48);
+            this.wordGuessButton.TabIndex = 9;
+            this.wordGuessButton.Text = "Guess Word";
+            this.wordGuessButton.UseVisualStyleBackColor = true;
+            this.wordGuessButton.Click += new System.EventHandler(this.wordGuessButton_Click);
+            // 
+            // realWordLabel
+            // 
+            this.realWordLabel.AutoSize = true;
+            this.realWordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.realWordLabel.Location = new System.Drawing.Point(505, 75);
+            this.realWordLabel.Name = "realWordLabel";
+            this.realWordLabel.Size = new System.Drawing.Size(109, 36);
+            this.realWordLabel.TabIndex = 10;
+            this.realWordLabel.Text = "WORD";
+            this.realWordLabel.Visible = false;
+            // 
+            // winOrLoseLabel
+            // 
+            this.winOrLoseLabel.AutoSize = true;
+            this.winOrLoseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.winOrLoseLabel.Location = new System.Drawing.Point(488, 28);
+            this.winOrLoseLabel.Name = "winOrLoseLabel";
+            this.winOrLoseLabel.Size = new System.Drawing.Size(138, 36);
+            this.winOrLoseLabel.TabIndex = 11;
+            this.winOrLoseLabel.Text = "You Win!";
+            this.winOrLoseLabel.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(42, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Guess a letter:";
+            // 
+            // lettersGuessed
+            // 
+            this.lettersGuessed.AutoSize = true;
+            this.lettersGuessed.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lettersGuessed.Location = new System.Drawing.Point(12, 75);
+            this.lettersGuessed.Name = "lettersGuessed";
+            this.lettersGuessed.Size = new System.Drawing.Size(0, 25);
+            this.lettersGuessed.TabIndex = 13;
+            this.lettersGuessed.Visible = false;
+            // 
+            // Hangman
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 644);
+            this.Controls.Add(this.lettersGuessed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.winOrLoseLabel);
+            this.Controls.Add(this.realWordLabel);
+            this.Controls.Add(this.wordGuessButton);
+            this.Controls.Add(this.wordInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.letterGuessButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.hangmanPicture);
+            this.Controls.Add(this.letterInput);
+            this.Name = "Hangman";
+            this.Text = "Hangman";
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox letterInput;
+        private System.Windows.Forms.PictureBox hangmanPicture;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button letterGuessButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox wordInput;
+        private System.Windows.Forms.Button wordGuessButton;
+        private System.Windows.Forms.Label realWordLabel;
+        private System.Windows.Forms.Label winOrLoseLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lettersGuessed;
+    }
+}
+
---
+++ Hangman/Hangman/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+    public partial class Hangman : Form
+    {
+        int wordLength, wordSelector, strikeCount, displayedCount, checkCount = 0, letterValue;
+        string realWord, displayed, letterString, wordBreaker;
+        char letter;
+        char[] wordAsChars, realWordAsChars;
+        List<string> fourLetter = new List<string> { "able", "acid", "baby", "ball", "band", "bear", "beat", "bell", "belt", "bird", "boat", "bomb", "bone", "book", "call", "card", "cell", "cook", "core", "dark", "data", "dawn", "desk", "easy", "exit", "farm", "fill", "game", "gulf", "half", "hero", "iron", "item", "join", "jump", "keen", "kick", "lock", "luck", "mass", "moon", "neck", "nine", "once", "open", "palm", "pipe", "rare", "root", "sake", "slip", "tale", "turn", "upon", "user", "vice", "vote", "weak", "wine", "yard", "year", "zero", "zone"};
+        List<string> fiveLetter = new List<string> { "abyss", "augur", "bacon", "burst", "cable", "clash", "death", "digit", "earth", "ember", "fable", "forms", "ghost", "grave", "hedge", "hotel", "index", "issue", "jewel", "joint", "knees", "knife", "laser", "loans", "magic", "melee", "nerve", "notch", "oasis", "oxide", "patch", "pixel", "quest", "quote", "ranch", "route", "satin", "scrap", "tanks", "tempo", "ultra", "urban", "vault", "vinyl", "waste", "witch", "yacht", "yards", "zone"};
+        List<string> sixLetter = new List<string> { "agenda", "amount", "beyond", "bureau", "castle", "combat", "debate", "domain", "effort", "engine", "fallen", "future", "garden", "guilty", "handle", "honest", "income", "invest", "jersey", "junior", "killed", "leader", "legacy", "marine", "modern", "narrow", "nature", "office", "origin", "phrase", "profit", "random", "region", "screen", "server", "target", "timing", "unique", "update", "version", "visual", "winter", "worker", "yellow"};
+        Boolean letterCheck;
+        Random rnd = new Random();
+
+        //checks if there was a change of input in the letter text box
+        private void letterInput_TextChanged(object sender, EventArgs e)
+        {
+            //checks if the text box is empty
+            if (string.IsNullOrEmpty(letterInput.Text))
+            {
+                //sets the letter guess button to false if the text box is empty
+                letterGuessButton.Enabled = false;
+            }
+            else
+            {
+                //converts the input to a char
+                letter = Convert.ToChar(letterInput.Text);
+                //converts the char to it's ASCII value
+                letterValue = Convert.ToInt32(letter);
+                //check if it's ASCII value is that of a letter
+                if (letterValue >= 65 & letterValue <= 90 || letterValue >= 97 & letterValue <= 122)
+                {
+                    //let's you guess it if it's actually a letter
+                    letterGuessButton.Enabled = true;
+                }
+                
+            }
+        }
+
+        //checks if there was a change of input in the word text box
+        private void wordInput_TextChanged(object sender, EventArgs e)
+        {
+            //checks if the word you typed is the same length as the real word
+            if (wordInput.TextLength == realWord.Length)
+            {
+                //if it is it'll let you guess it
+                wordGuessButton.Enabled = true;
+            }
+        }
+        public Hangman()
+        {
+            InitializeComponent();
+        }
+
+        //starts the game
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //enables the letter input
+            letterInput.Enabled = true;
+            //enables the word input
+            wordInput.Enabled = true;
+
+            //choses a word length between 4 and 6 at random
+            wordLength = rnd.Next(4, 6);
+            //runs to make the _ display for how many letters are in the word
+            while (displayedCount < wordLength)
+            {
+                // adds onto itself while leaving a space between the _
+                displayed = displayed + " _";
+                //makes it so the loop breaks when the right amount of _ have been placed
+                displayedCount++;
+            }
+
+            //changes the label to all the _
+            realWordLabel.Text = displayed;
+            //displays the _ to the player
+            realWordLabel.Visible = true;
+            //checks if the word is 4 letters long
+            if (wordLength == 4)
+            {
+                //selects a word at random from the set 4 letter words
+                wordSelector = rnd.Next(0, fourLetter.Count);
+                realWord = fourLetter[wordSelector];
+            }
+            //checks if the word is 5 letters long
+            else if (wordLength == 5)
+            {
+                //selects a word at random from the set 5 letter words
+                wordSelector = rnd.Next(0, fiveLetter.Count);
+                realWord = fiveLetter[wordSelector];
+            }
+            //checks if the word is 6 letters long
+            else if (wordLength == 6)
+            {
+                //selects a word at random from the set 6 letter words
+                wordSelector = rnd.Next(0, sixLetter.Count);
+                realWord = sixLetter[wordSelector];
+            }
+
+            //changes the amount of letters you can put into the word guessing text box to match how long the word is
+            wordInput.MaxLength = realWord.Length;
+
+            //makes a char array out of the display
+            wordAsChars = displayed.ToCharArray();
+            //breaks the actual word into chars
+            realWordAsChars = realWord.ToCharArray();
+            //stops you from starting a new game
+            startButton.Enabled = false;
+            //enables the reset button
+            resetButton.Enabled = true;
+        }
+        
+        //restes all the values in the game that needs to be reset to start a new game
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            startButton.Enabled = true;
+            resetButton.Enabled = true;
+            letterInput.Enabled = false;
+            wordInput.Enabled = false;
+            letterGuessButton.Enabled = false;
+            wordGuessButton.Enabled = false;
+            realWordLabel.Text = null;
+            realWordLabel.Visible = false;
+            winOrLoseLabel.Visible = false;
+            strikeCount = 0;
+            displayedCount = 0;
+            wordAsChars = null;
+            realWordAsChars = null;
+            displayed = null;
+            hangmanPicture.Image = new Bitmap("C:\\Users\\aidan\\source\\repos\\Hangman\\Hangman\\Resources\\base.png");
+
+        }
+
+        //lets you guess a letter
+        private void letterGuessButton_Click(object sender, EventArgs e)
+        {
+            //resets the check count
+            checkCount = 0;
+            //makes a string equal the letter input
+            letterString = letterInput.Text;
+            //converts the string to a lowercase char
+            letter = Convert.ToChar(letterString.ToLower());
+            //checks if the char array contains the letter
+            letterCheck = realWord.Contains(letter);
+            //if it does it runs this
+            if (letterCheck == true)
+            {
+                //runs through the whole word to determine where the matching char is
+                while (checkCount + 1 <= wordLength)
+                {
+                    //converts the the chosen char for each run to a string
+                    wordBreaker = Convert.ToString(realWordAsChars[checkCount]);
+                    
+                    //adds on to the count
+                    checkCount = checkCount + 1;
+                    //checks if the current letter matched the one guessed
+                    if (wordBreaker == letterString)
+                    {
+                        //changes the char array used for display to have the letter instead of _
+                        wordAsChars[checkCount*2-1] = letter;
+                    }
+                    
+                }
+                //updates the display label so that it can show where the correct letter you have guessed is
+                string displayedUpdate = new String(wordAsChars);
+                realWordLabel.Text = displayedUpdate;
+
+            }
+            //otherwise it runs this
+            else
+            {
+                //checks how many strikes you have and while chnage the image appropiatley as well as update your strike value
+                if (strikeCount == 0)
+                {
+                    strikeCount = 1;
+                    hangmanPicture.Image = new Bitmap("C:\\Users\\aidan\\source\\repos\\Hangman\\Hangman\\Resources\\oneStrike.png");
+                }
+                else if (strikeCount == 1)
+                {
+                    strikeCount = 2;
+                    hangmanPicture.Image = new Bitmap("C:\\Users\\aidan\\source\\repos\\Hangman\\Hangman\\Resources\\twoStrike.png");
+                }
+                else if (strikeCount == 2)
+                {
+                    strikeCount = 3;
+                    hangmanPicture.Image = new Bitmap("C:\\Users\\aidan\\source\\repos\\Hangman\\Hangman\\Resources\\threeStrike.png");
+                }
+                else if (strikeCount == 3)
+                {
+                    strikeCount = 4;
+                    hangmanPicture.Image = new Bitmap("C:\\Users\\aidan\\source\\repos\\Hangman\\Hangman\\Resources\\fourStrike.png");
+                }
+                else if (strikeCount == 4)
+                {
+                    strikeCount = 5;
+                    hangmanPicture.Image = new Bitmap("C:\\Users\\aidan\\source\\repos\\Hangman\\Hangman\\Resources\\fiveStrike.png");
+                }
+                //shows you that you lost if you hit too many strikes
+                else if (strikeCount == 5)
+                {
+                    strikeCount = 6;
+                    hangmanPicture.Image = new Bitmap("C:\\Users\\aidan\\source\\repos\\Hangman\\Hangman\\Resources\\sixStrike.png");
+                    winOrLoseLabel.Text = "You Lose";
+                    winOrLoseLabel.Visible = true;
+                    letterInput.Enabled = false;
+                    wordInput.Enabled = false;
+                    letterGuessButton.Enabled = false;
+                    wordGuessButton.Enabled = false;
+                }
+            }
+            //clears the letter input after each letter guess so you don't have to
+            letterInput.Text = null;
+        }
+
+        //lets you guess a word
+        private void wordGuessButton_Click(object sender, EventArgs e)
+        {
+            checkCount = 0;
+            //checks if you entered the real word
+            if (wordInput.Text == realWord)
+            {
+                //displays it at the top for you to see it
+                while (checkCount + 1 <= wordLength)
+                {
+                    wordBreaker = Convert.ToString(realWordAsChars[checkCount]);
+                    checkCount = checkCount + 1;
+                    wordAsChars[checkCount * 2 - 1] = Convert.ToChar(wordBreaker);
+                }
+                string displayedUpdate = new String(wordAsChars);
+                realWordLabel.Text = displayedUpdate;
+                //lets you know that you won while disabling you from guessing anymore
+                winOrLoseLabel.Text = "You Win!";
+                winOrLoseLabel.Visible = true;
+                letterInput.Enabled = false;
+                wordInput.Enabled = false;
+                letterGuessButton.Enabled = false;
+                wordGuessButton.Enabled = false;
+            }
+            //runs the strike check just like the letter one if you didn't guess the word correctly
+            else
+            {
+                if (strikeCount == 0)
+                {
+                    strikeCount = 1;
+                    hangmanPicture.Image = new Bitmap("C:\\Users\\aidan\\source\\repos\\Hangman\\Hangman\\Resources\\oneStrike.png");
+                }
+                else if (strikeCount == 1)
+                {
+                    strikeCount = 2;
+                    hangmanPicture.Image = new Bitmap("C:\\Users\\aidan\\source\\repos\\Hangman\\Hangman\\Resources\\twoStrike.png");
+                }
+                else if (strikeCount == 2)
+                {
+                    strikeCount = 3;
+                    hangmanPicture.Image = new Bitmap("C:\\Users\\aidan\\source\\repos\\Hangman\\Hangman\\Resources\\threeStrike.png");
+                }
+                else if (strikeCount == 3)
+                {
+                    strikeCount = 4;
+                    hangmanPicture.Image = new Bitmap("C:\\Users\\aidan\\source\\repos\\Hangman\\Hangman\\Resources\\fourStrike.png");
+                }
+                else if (strikeCount == 4)
+                {
+                    strikeCount = 5;
+                    hangmanPicture.Image = new Bitmap("C:\\Users\\aidan\\source\\repos\\Hangman\\Hangman\\Resources\\fiveStrike.png");
+                }
+                else if (strikeCount == 5)
+                {
+                    strikeCount = 6;
+                    hangmanPicture.Image = new Bitmap("C:\\Users\\aidan\\source\\repos\\Hangman\\Hangman\\Resources\\sixStrike.png");
+                    winOrLoseLabel.Text = "You Lose";
+                    winOrLoseLabel.Visible = true;
+                    letterInput.Enabled = false;
+                    wordInput.Enabled = false;
+                    letterGuessButton.Enabled = false;
+                    wordGuessButton.Enabled = false;
+                }
+            }
+            //clears the word input text box
+            wordInput.Text = null;
+        }
+
+    }
+}
